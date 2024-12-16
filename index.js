bfda77c29f1b9b503bbeddfb998fb20f2225ae2b
altramax <ajayiezekiel559@gmail.com>
2024-12-16 20:30:01 +0100

    dashboard

---
+++ index.js
@@ -0,0 +1,7 @@
+const loginBtn = document.getElementById("login-btn");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+loginBtn.addEventListener("click", () => {
+  console.log("clicked");
+});
