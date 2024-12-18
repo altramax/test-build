3851bc7a6ac9559ba0135f84aaa082744fc75c9b
altramax <ajayiezekiel559@gmail.com>
2024-12-18 09:14:28 +0100

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const loginBtn = document.getElementById("login-btn");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
+
 loginBtn.addEventListener("click", () => {
   console.log("clicked");
 });
